084efdd09383c6e8470d62c0cd6c34715bf3b7fe
Jacqueline <jp@azure-architects.nl>
2016-03-17 09:19:45 +0100

    hash to object example

---
+++ hash.ps1
@@ -0,0 +1,21 @@
+#$hostname = hostname
+#for ($i=0;$i -lt 5; $i++) {  Add-Content .\computers.txt $hostname }
+$computers = Get-Content .\computers.txt
+$output = @();
+
+foreach($entry in $computers) {
+	$info = Get-WmiObject -Class Win32_OperatingSystem -Computername $entry
+	$bios = Get-WmiObject -Class Win32_Bios -Computername $entry
+	
+		$hash = @{ Name = $entry; 
+		                   fabrikant = $bios.manufacturer;
+ 			                serial = $info.serialnumber;
+			                registratienaam = $info.registereduser;
+			                datum = (Get-Date);
+		                    company = 'Contoso' }
+
+	$object = New-Object -TypeName psobject -Property $hash
+	$output += $object
+}
+
+$output | Export-Csv inventory.csv 
