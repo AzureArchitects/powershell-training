57bcfeb9cce0f7f3423859177fc3b8540b276030
Jacqueline <jp@azure-architects.nl>
2016-03-23 09:39:29 +0100

    sourcing

--- object-directly.ps1
+++ object-directly.ps1
@@ -1,25 +1,42 @@
+Function Get-LotsOfUsefullInfo {
+
 $hostname = hostname
-for ($i=0;$i -lt 5; $i++) {  Add-Content .\computers.txt $hostname }
-$computers = Get-Content .\computers.txt
+for ($i=0;$i -lt 5; $i++) {  Add-Content .\dumpcomputers.txt $hostname }
+$computers = Get-Content .\dumpcomputers.txt
 $output = @();
 
 foreach($entry in $computers) {
+    
+    $pspath = $PSVersionTable
+    $psversion = @()
+    foreach ($p in $pspath) {
+        
+        $psobject = [pscustomobject]@{
+            PSVersie = $p.PSVersion
+            WSMANVersie = $p.WSManStackVersion
+        }
+        
+        $psversion += $psobject
+        
+    } 
+   
+    
 	$info = Get-WmiObject -Class Win32_OperatingSystem -Computername $entry
 	$bios = Get-WmiObject -Class Win32_Bios -Computername $entry
-	$environment = [System.Environment]::GetEnvironmentVariables();
-    
+	   
        
-    $object = New-Object -TypeName psobject -Property @{
+    $object = [pscustomobject]@{
                             fabrikant = $bios.manufacturer;
  			                serial = $info.serialnumber;
 			                registratienaam = $info.registereduser;
 			                datum = (Get-Date);
                             company = 'Contoso';
-                            pad = $environment.Item("Path");
+                            powershell = $psversion;
     
         }
 	$output += $object
+    }
+
+    write-output $output 
+
 }
-
-$output | Export-Csv inventory2.csv -NoTypeInformation
-
