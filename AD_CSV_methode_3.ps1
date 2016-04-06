9e6efe2f9b842d585d9dbd002bb7beb09bad02d5
Azure Architects <jacqueline.portier@eic.nl>
2016-04-07 00:02:11 +0200

    Update AD_CSV_methode_3.ps1

--- AD_CSV_methode_3.ps1
+++ AD_CSV_methode_3.ps1
@@ -2,5 +2,9 @@
 
 
 foreach ($user  in $users){
-    New-ADUser -Path 'OU=NL,DC=MDS,DC=local' -Name $user.Name -StreetAddress $user.StreetAddress -Surname $user.SurName -Company $user.Company -Department $user.Department -City $user.City -State $user.State -PostalCode $user.PostalCode -HomePhone $user.HomePhone -MobilePhone $user.MobilePhone -EmailAddress $user.EmailAddress -HomePage $user.HomePage
+    New-ADUser -Path 'OU=NL,DC=MDS,DC=local' -Name $user.Name -StreetAddress $user.StreetAddress `
+     -Surname $user.SurName -Company $user.Company -Department $user.Department ` 
+     -City $user.City -State $user.State -PostalCode $user.PostalCode `
+     -HomePhone $user.HomePhone -MobilePhone $user.MobilePhone -EmailAddress $user.EmailAddress `
+     -HomePage $user.HomePage -accountPassword (ConvertTo-SecureString -AsPlainText "P@ssword2" -Force)
 }
