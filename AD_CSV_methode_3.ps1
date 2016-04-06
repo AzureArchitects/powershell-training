e2415f6fe5c1947fa60a3745c80309c733a48504
Azure Architects <jacqueline.portier@eic.nl>
2016-04-06 23:59:29 +0200

    Update AD_CSV_methode_3.ps1

--- AD_CSV_methode_3.ps1
+++ AD_CSV_methode_3.ps1
@@ -1,6 +1,6 @@
-﻿$users = Import-Csv -Path C:\vagrant\export-jelle.csv -Delimiter ";"
+﻿$users = Import-Csv -Path .\export.csv -Delimiter ";"
 
 
 foreach ($user  in $users){
-    New-ADUser -Path 'OU=USA,DC=kontoso,DC=com' -Name $user.Name -StreetAddress $user.StreetAddress -Surname $user.SurName -Company $user.Company -Department $user.Department -City $user.City -State $user.State -PostalCode $user.PostalCode -HomePhone $user.HomePhone -MobilePhone $user.MobilePhone -EmailAddress $user.EmailAddress -HomePage $user.HomePage
+    New-ADUser -Path 'OU=NL,DC=MDS,DC=local' -Name $user.Name -StreetAddress $user.StreetAddress -Surname $user.SurName -Company $user.Company -Department $user.Department -City $user.City -State $user.State -PostalCode $user.PostalCode -HomePhone $user.HomePhone -MobilePhone $user.MobilePhone -EmailAddress $user.EmailAddress -HomePage $user.HomePage
 }
